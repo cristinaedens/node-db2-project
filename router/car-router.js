dac44478c65f557afe68f91f0f771b2df269a272
Cristina Edens <edens.cristina@gmail.com>
2020-03-17 15:29:12 -0400

    added an index.js file set up car-route

---
+++ router/car-router.js
@@ -0,0 +1,10 @@
+const express = require('express'); //import express
+
+const db = require('../data/config'); //import the database
+
+const router = express.Router(); //declare
+
+
+
+
+module.exports = router; //export the router
